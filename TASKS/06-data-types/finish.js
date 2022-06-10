7fe0caeda822b1ee805c3a985c584437672718af
Viktar Baradzin <a0fragile@gmail.com>
2022-06-10 20:28:57 +0300

    init 1-10 completed

---
+++ TASKS/06-data-types/finish.js
@@ -0,0 +1,29 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+
+const string = "string"
+const number = 1
+const boolean = true
+const nul = null
+const undfnd = undefined
+const arr = [1,2,3]
+
+console.log(typeof(string))
+console.log(typeof(number))
+console.log(typeof(boolean))
+console.log(typeof(nul))
+console.log(typeof(undfnd))
+console.log(typeof(arr))
